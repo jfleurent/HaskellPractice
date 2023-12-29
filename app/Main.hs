dd1ba141168ca6ebc8d23c6af122cf761832b83a
jeffr <jeffreyfleurent@gmaill.com>
2023-12-28 22:40:23 -0500

    Problems Update
    - Added 2 problems and solutions

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main (main) where
 import PracticeProblems
 
 main :: IO ()
-main = print $ encode "aaaabccaadeeee"
+main = print $ decodeStringList ["Multiple 4 'a'","Single 'b'","Multiple 2 'c'","Multiple 2 'a'","Single 'd'","Multiple 4 'e'"]
--- out/production/my-project/PracticeProblems.hs
+++ out/production/my-project/PracticeProblems.hs
@@ -8,9 +8,13 @@ module PracticeProblems
     flatten,
     compress,
     pack,
-    encode
+    encode,
+    encodeToString,
+    decodeStringList
   )
 where
+
+import Data.Char (ord)
 
 --All problems are found here: https://wiki.haskell.org/H-99:_Ninety-Nine_Haskell_Problems
 ------------------------------------------------------------------------------------------------------------------------
@@ -18,8 +22,8 @@ where
 -- (*) Find the last element of a list.
 
 findLastElement :: [Int] -> String
-findLastElement [] =  "No such element"
-findLastElement list =  "Last element of list is : " ++ show (last list)
+findLastElement [] = "No such element"
+findLastElement list = "Last element of list is : " ++ show (last list)
 
 ------------------------------------------------------------------------------------------------------------------------
 -- !!!! Problem 2 !!!!
@@ -28,7 +32,7 @@ findLastElement list =  "Last element of list is : " ++ show (last list)
 findSecondToLastElement :: [Int] -> String
 findSecondToLastElement [] = "No such element"
 findSecondToLastElement [_] = "No such element"
-findSecondToLastElement (_:x:_) = "Second to last element is : " ++ show x
+findSecondToLastElement (_ : x : _) = "Second to last element is : " ++ show x
 
 ------------------------------------------------------------------------------------------------------------------------
 -- !!!! Problem 3 !!!!
@@ -36,38 +40,38 @@ findSecondToLastElement (_:x:_) = "Second to last element is : " ++ show x
 
 findKthElement :: Int -> [Int] -> String
 findKthElement index list
-                        | index < length list = "The element at index is : " ++ show(list !! index)
-                        | otherwise =  "No such element"
+  | index < length list = "The element at index is : " ++ show (list !! index)
+  | otherwise = "No such element"
 
- -----------------------------------------------------------------------------------------------------------------------
- -- !!!! Problem 4 !!!!
- -- (*) Find the number of elements in a list.
+-----------------------------------------------------------------------------------------------------------------------
+-- !!!! Problem 4 !!!!
+-- (*) Find the number of elements in a list.
 
 findNumberOfElements :: [Int] -> String
-findNumberOfElements list = "The number elements in list is : " ++ show(length list)
+findNumberOfElements list = "The number elements in list is : " ++ show (length list)
 
- -----------------------------------------------------------------------------------------------------------------------
- -- !!!! Problem 5 !!!!
+-----------------------------------------------------------------------------------------------------------------------
+-- !!!! Problem 5 !!!!
 -- (*) Reverse a list.
 
 reverseAList :: [Int] -> String
-reverseAList list = "The list reversed is : " ++ show(reverse list)
+reverseAList list = "The list reversed is : " ++ show (reverse list)
 
- -----------------------------------------------------------------------------------------------------------------------
- -- !!!! Problem 6 !!!!
+-----------------------------------------------------------------------------------------------------------------------
+-- !!!! Problem 6 !!!!
 -- (*) Find out whether a list is a palindrome.
 
 isPalindrome :: String -> Bool
-isPalindrome string = and [a == b | (a,b) <- zip string (reverse string)]
+isPalindrome string = and [a == b | (a, b) <- zip string (reverse string)]
 
 determinePalindrome :: String -> String
-determinePalindrome string = "String " ++ string ++ " is palindrome? : " ++ show(isPalindrome string)
+determinePalindrome string = "String " ++ string ++ " is palindrome? : " ++ show (isPalindrome string)
 
 ------------------------------------------------------------------------------------------------------------------------
- -- !!!! Problem 7 !!!!
- -- (**) Flatten a nested list structure.
- 
- -- TODO Figure out how this structure works
+-- !!!! Problem 7 !!!!
+-- (**) Flatten a nested list structure.
+
+-- TODO Figure out how this structure works
 --data NestedList a = Elem a | List[NestedList a]
 --
 --list :: NestedList Integer
@@ -83,17 +87,17 @@ flatten = concat
 compress :: String -> String
 compress [] = []
 compress [x] = [x]
-compress (x1:x2:xs)
-             | x1 == x2 = compress (x2:xs)
-             | otherwise = x1 : compress (x2:xs)
-             
+compress (x1 : x2 : xs)
+  | x1 == x2 = compress (x2 : xs)
+  | otherwise = x1 : compress (x2 : xs)
+
 ------------------------------------------------------------------------------------------------------------------------
 -- !!!! Problem 9 !!!!
 -- (**) Pack consecutive duplicates of list elements into sublists.
 
 pack :: String -> [String]
 pack [] = []
-pack (x:xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
+pack (x : xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
 
 ------------------------------------------------------------------------------------------------------------------------
 -- !!!! Problem 10 !!!!
@@ -101,4 +105,36 @@ pack (x:xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
 
 encode :: String -> [(Int, Char)]
 encode s = zip (map length l) (map head l)
-            where l = pack s
+  where
+    l = pack s
+
+-----------------------------------------------------------------------------------------------------------------------
+-- !!!! Problem 11 !!!!
+-- (*) Modified run-length encoding.
+
+encodedMessage :: (Int, Char) -> String
+encodedMessage m
+  | fst m > 1 = "Multiple " ++ show (fst m) ++ " " ++ show (snd m)
+  | fst m == 1 = "Single " ++ show (snd m)
+  | otherwise = ""
+
+encodeToString :: String -> [String]
+encodeToString s = map encodedMessage $ encode s
+
+-----------------------------------------------------------------------------------------------------------------------
+-- !!!! Problem 12 !!!!
+-- (**) Decode a run-length encoded list.
+
+generateString :: Char -> Int -> String
+generateString _ 0 = ""
+generateString c x = c : generateString c (x - 1)
+
+decodeString :: String -> String
+decodeString [] = ""
+decodeString (s : ss)
+  | s `elem` "M" = generateString (ss !! 11)  ((ord (ss !! 8)) - 48)
+  | s `elem` "S" = generateString (ss !! 7) 1
+  | otherwise = ""
+
+decodeStringList :: [String] -> String
+decodeStringList s = concat $ map decodeString s
